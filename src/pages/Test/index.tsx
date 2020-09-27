1ba79699154dae90117fd25c95b93d073a76899c
zhoutian <96166832@qq.com>
2020-09-27 16:06:56 +0800

    no message

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -1,27 +1,21 @@
 
 import React from 'react';
 import { Button, Modal } from 'antd';
-import { AsyncButton } from 'raui';
 
 export default function test() {
   const onClick = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        Modal.open({
-          title: '11',
-          content: '22',
-          onCancel: () => {
+    Modal.open({
+      title: '11',
+      content: '22',
+      onCancel: () => {
 
-          },
-        });
-        resolve();
-      }, 2000)
-    })
+      },
+    });
   };
-  
+
   return (
     <>
-      <AsyncButton onClick={onClick}>点击</AsyncButton>
+      <Button onClick={onClick}>点击</Button>
     </>
   );
 }
